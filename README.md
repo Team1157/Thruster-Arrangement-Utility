8f62814c5ac9fc85729e37453cb9bc97f12a5f12
Noah Mollerstuen <noah@mollerstuen.com>
2021-08-30 18:15:53 -0600

    Added a second plot showing torque

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ This program was initially created with the design of underwater ROVs in mind, b
 Note that the coordinate system for this program uses aircraft-style coordinates rather than conventional ones. ![image](https://user-images.githubusercontent.com/43499473/129992017-ad34299f-88f0-4ae0-800b-cbe1d22d72d5.png)
 
 TODO:
- - add mode for max torque output at zero thrust
  - add a GUI
  - make the output surface less jagged
  - add a way to show individual thruster values at a given point
+ - make sure the thrusters are sharing load equally
+ - accurately show surfaces when the vehicle has fewer than 6 dof
--- tau.py
+++ tau.py
@@ -37,25 +37,20 @@ class Thruster3D:
         return np.cross(self.pos, self.orientation)
 
 
-def transform_orientations(thrusters: t.List[Thruster3D], target_dir: np.ndarray):
+def rotate_to_vector(vectors: np.ndarray, target_dir: np.ndarray) -> np.ndarray:
     """
-    Calculate the maximum force achievable in (exclusively) the given direction by the given set of constraints
-    :param thrusters: A list of Thruster3D objects representing the available thrusters
+    Rotate a group of vectors so that the a specified vector is along the +x axis
+    :param vectors: A 2d numpy array in which each column is a 3d vector
     :param target_dir: A 3d vector in the target direction
-    :return: The maximum thrust force in kgf
+    :return: A np array with the same size as vectors, with the same rotation applied to each column
     """
     target_dir = target_dir / np.linalg.norm(target_dir)  # Make target_dir a unit vector
-
-    orientations = np.empty((3, len(thrusters)))  # Create an empty 2d array to hold the orientation of each thruster
-    for i in range(len(thrusters)):
-        thruster = thrusters[i]
-        orientations[..., i] = thruster.orientation
 
     new_bases = np.empty((3, 3))  # Create an empty 3x3 change of basis matrix
     new_bases[..., 0] = target_dir  # The first basis is our target direction
 
     if not (target_dir[1] == 0 and target_dir[2] == 0):  # Make sure the cross product computed below isn't 0
-        second_basis = np.cross(target_dir, np.array([1, 0, 0]))  # Choose a second basis parallel the first
+        second_basis = np.cross(target_dir, np.array([1, 0, 0]))  # Choose a second basis perpendicular the first
     else:
         second_basis = np.cross(target_dir, np.array([0, 1, 0]))
     second_basis /= np.linalg.norm(second_basis)  # Make the second basis a unit vector
@@ -70,45 +65,43 @@ def transform_orientations(thrusters: t.List[Thruster3D], target_dir: np.ndarray
     inverse_transform = np.linalg.inv(new_bases)
 
     # Calculate the transformation with matrix_vector multiplication
-    transformed_orientations = inverse_transform.dot(orientations)
+    transformed_orientations = inverse_transform.dot(vectors)
 
     return transformed_orientations
 
 
-def get_max_thrust(thrusters, transformed_orientations, torques: np.ndarray, max_current: int):
-    thruster_count = len(torques)
-
+def get_max_effort(thrusters: t.List[Thruster3D], objective: np.ndarray, constraints: np.ndarray,
+                   max_current: int):
     # First Simplex run. Find the maximum thrust in the desired direction
-    objective = -transformed_orientations[0]  # Get the max thrust in the transformed x axis (the target direction)
-    # All other thrusts and torques must be 0
-    torques_transpose = torques.transpose()
-    left_of_equality = np.row_stack((torques_transpose, transformed_orientations[1:]))
-
     bounds = [thruster.max_thrusts for thruster in thrusters]
 
     right_of_equality = np.zeros(5)  # There are 5 constraints that must all be 0
 
-    max_thrust_result = linprog(c=objective, A_ub=None, b_ub=None, A_eq=left_of_equality, b_eq=right_of_equality,
+    max_effort_result = linprog(c=-objective, A_ub=None, b_ub=None, A_eq=constraints, b_eq=right_of_equality,
                                 bounds=bounds, method="highs")
 
-    max_thrust = -.999 * max_thrust_result.fun  # some sort of precision/numerical error makes this bullshit necessary
+    max_effort = -.999 * max_effort_result.fun  # some sort of precision/numerical error makes this bullshit necessary
 
-    # Second Simplex run. Find the minimum current that produces the same thrust as the first result
+    if max_effort < 0.00000001:
+        # The thruster layout is incapable of producing effort in the target direction
+        return 0.0
+
+    # Second Simplex run. Find the minimum current that produces the same effort as the first result
 
     # Each thruster is split into reverse and forwards, so there are double the elements in the objective
-    # Minimize the sum of the absolute value of each thrust
-    objective_mincurrent = np.concatenate((-np.ones(thruster_count), np.ones(thruster_count)))
+    # Minimize the sum of the absolute value of each effort
+    objective_mincurrent = np.concatenate((-np.ones(len(thrusters)), np.ones(len(thrusters))))
 
     # All 6 degrees of freedom are constrained
-    thruster_constraints_mincurrent = np.row_stack((transformed_orientations, torques_transpose))
+    thruster_constraints_mincurrent = np.row_stack((objective, constraints))
     # Each thruster is split in two, the constraints are the same for each half of a thruster
     left_of_equality_mincurrent = np.column_stack((thruster_constraints_mincurrent, thruster_constraints_mincurrent))
 
-    bounds_mincurrent = [(DEFAULT_MAX_THRUSTS[0], 0.0) for _ in range(thruster_count)] + \
-                        [(0.0, DEFAULT_MAX_THRUSTS[1]) for _ in range(thruster_count)]
+    bounds_mincurrent = [(DEFAULT_MAX_THRUSTS[0], 0.0) for _ in range(len(thrusters))] + \
+                        [(0.0, DEFAULT_MAX_THRUSTS[1]) for _ in range(len(thrusters))]
 
     right_of_equality_mincurrent = [
-        max_thrust,  # x thrust constrained to previous maximum
+        max_effort,  # x thrust constrained to previous maximum
         0,  # y thrust
         0,  # z thrust
         0,  # x torque
@@ -122,9 +115,9 @@ def get_max_thrust(thrusters, transformed_orientations, torques: np.ndarray, max
     min_current_duplicated_array = min_current_result.x
 
     min_current_true_array = []
-    for i in range(0, thruster_count):
+    for i in range(0, len(thrusters)):
         min_current_true_array.append(min_current_duplicated_array[i] + min_current_duplicated_array[
-            i + thruster_count])  # combine half-thrusters into full thrusters
+            i + len(thrusters)])  # combine half-thrusters into full thrusters
 
     current_quadratic = [0] * 3
 
@@ -142,50 +135,43 @@ def get_max_thrust(thrusters, transformed_orientations, torques: np.ndarray, max
     current_quadratic[2] -= max_current  # ax^2 + bx + c = I -> ax^2 + bx + (c-I) = 0
 
     # solve quadratic, take the proper point, and clamp it to a maximum of 1.0
-    thrust_multiplier = min(1., max(np.roots(current_quadratic)))
+    effort_multiplier = min(1., max(np.roots(current_quadratic)))
 
-    return max_thrust * thrust_multiplier
+    return max_effort * effort_multiplier
 
 
-#####################################
-# Yaw, pitch, roll code
-#####################################
-def calc_max_yaw_pitch_roll(thrusters, thruster_torques):
-    torque_x = []
-    torque_y = []
-    torque_z = []
-    orientation = []
-    constraint3 = []
-    constraint4 = []
-    constraint5 = []
-    right_equalities = [0, 0, 0, 0, 0]
-    bounds = []
+def setup_subplot(subplot, thrusters, axes_bounds):
+    subplot.set_box_aspect((1, 1, 1))
+    subplot.view_init(elev=30, azim=-150)
+    subplot.set_xlim((axes_bounds, -axes_bounds))  # Invert x axis
+    subplot.set_ylim((-axes_bounds, axes_bounds))
+    subplot.set_zlim((axes_bounds, -axes_bounds))  # Invert z axis
+    # Draw some "axes" so it's clear where (0, 0, 0) is
+    subplot.plot((-axes_bounds, axes_bounds), (0, 0), (0, 0), c="black")
+    subplot.plot((0, 0), (-axes_bounds, axes_bounds), (0, 0), c="black")
+    subplot.plot((0, 0), (0, 0), (-axes_bounds, axes_bounds), c="black")
+    # Plot the locations and orientations of the thrusters
+    thrusterloc_x = [2 * thruster.pos[0] for thruster in thrusters]
+    thrusterloc_y = [2 * thruster.pos[1] for thruster in thrusters]
+    thrusterloc_z = [2 * thruster.pos[2] for thruster in thrusters]
+    thrusterdir_x = [2 * thruster.orientation[0] for thruster in thrusters]
+    thrusterdir_y = [2 * thruster.orientation[1] for thruster in thrusters]
+    thrusterdir_z = [2 * thruster.orientation[2] for thruster in thrusters]
+    subplot.quiver(thrusterloc_x, thrusterloc_y, thrusterloc_z, thrusterdir_x, thrusterdir_y, thrusterdir_z,
+                   color="black")
 
-    for i in range(len(thruster_torques)):
-        torque_x.append(thruster_torques[i][0])
-        torque_y.append(thruster_torques[i][1])
-        torque_z.append(thruster_torques[i][2])
-        thruster = thrusters[i]
-        orientation.append(thruster.orientation)
-        constraint3.append(thruster.orientation[0])
-        constraint4.append(thruster.orientation[1])
-        constraint5.append(thruster.orientation[2])
-        bounds.append(thrusters[i].max_thrusts)
 
-    for i in range(3):
-        torques = [torque_x, torque_y, torque_z]
-        objective = [torques[i]]
-
-        torques.pop(i)
-        constraint1 = torques[0]
-        constraint2 = torques[1]
-
-        left_equalities = [constraint1, constraint2, constraint3, constraint4, constraint5]
-
-        res = linprog(c=objective, A_ub=None, b_ub=None, A_eq=left_equalities, b_eq=right_equalities,
-                      bounds=bounds, method="highs")
-
-        print(res)
+def add_colorbar(plot, ax, color_index):
+    color_range = color_index.max() - color_index.min()
+    norm = matplotlib.colors.Normalize(vmin=color_index.min(), vmax=color_index.max())
+    m = cm.ScalarMappable(cmap=plt.cm.jet, norm=norm)
+    plot.colorbar(m, ticks=[
+        color_index.min(),
+        color_index.min() + color_range * 1 / 4,
+        color_index.min() + color_range * 2 / 4,
+        color_index.min() + color_range * 3 / 4,
+        color_index.max()
+    ], ax=ax, fraction=0.1, shrink=0.5)
 
 
 # The main entry point of the program
@@ -224,95 +210,113 @@ def main(thrusters, resolution: int, max_current: int):
         for thruster_raw in thrusters_raw
     ]
 
-    # Calculate the torque constrains which will apply to every iteration
-    thruster_torques = np.array([thruster.torque() for thruster in thrusters])
+    # Format the orientation and torque of the thrusters to be used as constraints
+    thruster_orientations = np.array([thruster.orientation for thruster in thrusters]).transpose()
+    thruster_torques = np.array([thruster.torque() for thruster in thrusters]).transpose()
 
     # I have no idea what np.meshgrid does
     u, v = np.mgrid[0:2 * np.pi:resolution * 1j, 0:np.pi: resolution / 2 * 1j]
-    np.empty(np.shape(u))
-    mesh_x = np.empty(np.shape(u))
-    mesh_y = np.empty(np.shape(u))
-    mesh_z = np.empty(np.shape(u))
-    color_index = np.empty(np.shape(u))
+    thrust_mesh_x = np.empty(np.shape(u))
+    thrust_mesh_y = np.empty(np.shape(u))
+    thrust_mesh_z = np.empty(np.shape(u))
+    thrust_color_index = np.empty(np.shape(u))
+    torque_mesh_x = np.empty(np.shape(u))
+    torque_mesh_y = np.empty(np.shape(u))
+    torque_mesh_z = np.empty(np.shape(u))
+    torque_color_index = np.empty(np.shape(u))
 
     # Iterate over each vertex and calculate the max thrust in that direction
     # Note: Should probably be its own function, then it can be optimized more (i.e. Numba)
-    max_rho = 0
+    max_thrust = 0
+    max_torque = 0
     for i in range(np.shape(u)[0]):
         for j in range(np.shape(u)[1]):
             z = np.cos(u[i][j]) * np.sin(v[i][j])
             y = np.sin(u[i][j]) * np.sin(v[i][j])
             x = np.cos(v[i][j])
-            transformed_orientations = transform_orientations(thrusters, np.array([x, y, z]))
-            rho = get_max_thrust(thrusters, transformed_orientations, thruster_torques, max_current)
-            mesh_x[i][j] = x * rho
-            mesh_y[i][j] = y * rho
-            mesh_z[i][j] = z * rho
-            color_index[i][j] = rho
-            max_rho = max(max_rho, rho)
+            transformed_orientations = rotate_to_vector(thruster_orientations, np.array([x, y, z]))
+            transformed_torques = rotate_to_vector(thruster_torques, np.array([x, y, z]))
 
-    max_rho = np.ceil(max_rho)
+            thrust = get_max_effort(thrusters, transformed_orientations[0],
+                                    np.row_stack((transformed_orientations[1:], thruster_torques)), max_current)
+            torque = get_max_effort(thrusters, transformed_torques[0],
+                                    np.row_stack((transformed_torques[1:], thruster_orientations)), max_current)
 
-    norm = matplotlib.colors.Normalize(vmin=color_index.min(), vmax=color_index.max())
+            thrust_mesh_x[i][j] = x * thrust
+            thrust_mesh_y[i][j] = y * thrust
+            thrust_mesh_z[i][j] = z * thrust
+            thrust_color_index[i][j] = thrust
+
+            torque_mesh_x[i][j] = x * torque
+            torque_mesh_y[i][j] = y * torque
+            torque_mesh_z[i][j] = z * torque
+            torque_color_index[i][j] = torque
+
+            max_thrust = max(max_thrust, thrust)
+            max_torque = max(max_torque, torque)
 
     # Start plotting results
     matplotlib.use('TkAgg')
-    fig = plt.figure()
+    fig = plt.figure(figsize=(12, 6))  # Window size, in inches for some reason
 
     # Set up plot: 3d orthographic plot with ROV axis orientation
-    ax = fig.add_subplot(111, projection='3d', proj_type='ortho')
+    ax_thrust = fig.add_subplot(121, projection='3d', proj_type='ortho')
+    ax_torque = fig.add_subplot(122, projection='3d', proj_type='ortho')
 
-    ax.set_box_aspect((1, 1, 1))
-    ax.view_init(elev=30, azim=-150)
+    setup_subplot(ax_thrust, thrusters, np.ceil(max_thrust))
+    ax_thrust.set_xlabel('X (Surge)')
+    ax_thrust.set_ylabel('Y (Sway)')
+    ax_thrust.set_zlabel('Z (Heave)')
+    ax_thrust.title.set_text('Thrust')
 
-    ax.set_xlim((max_rho, -max_rho))  # Invert x axis
-    ax.set_ylim((-max_rho, max_rho))
-    ax.set_zlim((max_rho, -max_rho))  # Invert z axis
+    setup_subplot(ax_torque, thrusters, np.ceil(max_torque))
+    ax_torque.set_xlabel('X (Roll)')
+    ax_torque.set_ylabel('Y (Pitch)')
+    ax_torque.set_zlabel('Z (Yaw)')
+    ax_torque.title.set_text('Torque')
 
-    ax.set_xlabel('X (Surge)')
-    ax.set_ylabel('Y (Sway)')
-    ax.set_zlabel('Z (Heave)')
+    # Synchronize the rotation and zoom of both subplots
+    def on_plot_move(event):
+        if event.inaxes is None:
+            return
+        ax = event.inaxes
+        ax2 = ax_thrust if event.inaxes == ax_torque else ax_torque
+        try:
+            button_pressed = ax.button_pressed
+        except AttributeError:
+            return
+        if button_pressed in ax._rotate_btn:
+            ax2.view_init(elev=ax.elev, azim=ax.azim)
+        elif button_pressed in ax._zoom_btn:
+            ax2.set_xlim3d(ax.get_xlim3d())
+            ax2.set_ylim3d(ax.get_ylim3d())
+            ax2.set_zlim3d(ax.get_zlim3d())
+        fig.canvas.draw_idle()
 
-    # Draw some "axes" so it's clear where (0, 0, 0) is
-    ax.plot((-max_rho, max_rho), (0, 0), (0, 0), c="black")
-    ax.plot((0, 0), (-max_rho, max_rho), (0, 0), c="black")
-    ax.plot((0, 0), (0, 0), (-max_rho, max_rho), c="black")
+    fig.canvas.mpl_connect('motion_notify_event', on_plot_move)
 
-    # Plot the locations and orientations of the thrusters
-    # NOTE: Consider merging this all into the function call below to avoid creating all these unwieldy variable names?
-    thrusterloc_x = [2 * thruster.pos[0] for thruster in thrusters]
-    thrusterloc_y = [2 * thruster.pos[1] for thruster in thrusters]
-    thrusterloc_z = [2 * thruster.pos[2] for thruster in thrusters]
+    # Adjust each color so that the min and max values correspond to the min and max colors
+    thrust_color_index_modified = (thrust_color_index - thrust_color_index.min()) / \
+                                  (thrust_color_index.max() - thrust_color_index.min())
+    torque_color_index_modified = (torque_color_index - torque_color_index.min()) / \
+                                  (torque_color_index.max() - torque_color_index.min())
 
-    thrusterdir_x = [2 * thruster.orientation[0] for thruster in thrusters]
-    thrusterdir_y = [2 * thruster.orientation[1] for thruster in thrusters]
-    thrusterdir_z = [2 * thruster.orientation[2] for thruster in thrusters]
-
-    ax.quiver(thrusterloc_x, thrusterloc_y, thrusterloc_z, thrusterdir_x, thrusterdir_y, thrusterdir_z, color="black")
-
-    # Plot the zero-torque maximum thrust in each direction
-    color_index_modified = (color_index - color_index.min()) / (color_index.max() - color_index.min())
-    ax.plot_surface(
-        mesh_x, mesh_y, mesh_z,
-        alpha=0.6, facecolors=cm.jet(color_index_modified), edgecolors='w', linewidth=0
+    # Plot the surfaces on their respective subplots
+    ax_thrust.plot_surface(
+        thrust_mesh_x, thrust_mesh_y, thrust_mesh_z,
+        alpha=0.6, facecolors=cm.jet(thrust_color_index_modified), edgecolors='w', linewidth=0
+    )
+    ax_torque.plot_surface(
+        torque_mesh_x, torque_mesh_y, torque_mesh_z,
+        alpha=0.6, facecolors=cm.jet(torque_color_index_modified), edgecolors='w', linewidth=0
     )
 
     # Create a legend mapping the colors of the thrust plot to thrust values
-    color_range = color_index.max() - color_index.min()
-    m = cm.ScalarMappable(cmap=plt.cm.jet, norm=norm)
-    plt.colorbar(m, ticks=[
-        color_index.min(),
-        color_index.min() + color_range / 4,
-        color_index.min() + color_range / 2,
-        color_index.min() + 3 * color_range / 4,
-        color_index.max()
-    ])
+    add_colorbar(plt, ax_thrust, thrust_color_index)
+    add_colorbar(plt, ax_torque, torque_color_index)
 
     # Show plot
     plt.show()
-
-    # Print max yaw, pitch, and roll
-    calc_max_yaw_pitch_roll(thrusters, thruster_torques)
 
 
 if __name__ == "__main__":  # Only run the main function the program is being run directly, not imported
